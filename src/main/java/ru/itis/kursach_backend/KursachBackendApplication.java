7955b287f63a7b6aa23a9ddae90da9f2fc2cef18
Damir <damir17022000@gmail.com>
2020-03-13 17:57:21 +0300

    Initial commit

---
+++ src/main/java/ru/itis/kursach_backend/KursachBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.kursach_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursachBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursachBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itis/kursach_backend/KursachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.kursach_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursachBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
