cd964580caae59bf6a00834d3e2ff9de7bec1f80
Damir <damir17022000@gmail.com>
2020-05-28 00:27:25 +0300

    ready for deploy fix 2 final

--- src/main/java/ru/itis/kursach/KursachApplication.java
+++ src/main/java/ru/itis/kursach/KursachApplication.java
@@ -2,11 +2,18 @@ package ru.itis.kursach;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class KursachApplication {
+public class KursachApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(KursachApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KursachApplication.class, args);
