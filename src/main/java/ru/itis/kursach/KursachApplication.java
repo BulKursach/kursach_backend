4c7a6883822b842e7eab1bbb83f3602cc22a401d
Damir <damir17022000@gmail.com>
2020-05-28 20:44:28 +0300

    final fixes

--- src/main/java/ru/itis/kursach/KursachApplication.java
+++ src/main/java/ru/itis/kursach/KursachApplication.java
@@ -29,7 +29,7 @@ public class KursachApplication extends SpringBootServletInitializer {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins("http://5.53.125.224::3000");
             }
         };
     }
